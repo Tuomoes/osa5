85248dfa30b49110da0f1cc28c713d51dc07816f
DESKTOP-9H4TH8F\tuomo <tuomo_eskola@hotmail.com>
2018-08-17 10:29:29 +0300

    Subtasks 5.15 and 5.16* done

--- src/App.test.js
+++ src/App.test.js
@@ -13,11 +13,14 @@ describe('<App />', () => {
         app = mount(<App />)
     })
 
-    it('renders the login title before user has been logged in', () => {
+    it('renders the login title before user has been logged in. Does not render the blogs', () => {
         app.update()
-
+        const blogComponents = app.find(Blog)
         const loginTitle = app.find('.loginTitle')
         expect(loginTitle.text()).toContain('Login to application')
+        console.log('blog components length is:', blogComponents.length)
+        console.log('blog service blogs length is:', blogService.blogs.length)
+        expect(blogComponents.length).toEqual(0)
     })
 
     it('after login blog count equals 2', async () => {

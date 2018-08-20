fb4116fc12d1cd4ea83a527ec543e6ca21556c78
DESKTOP-9H4TH8F\tuomo <tuomo_eskola@hotmail.com>
2018-08-20 14:53:16 +0300

    Subtask 5.20* done

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote=>
+        {anecdotes.sort((a, b) => { return (b.votes - a.votes)}).map(anecdote=>
           <div key={anecdote.id}>
             <div>
               {anecdote.content} 
